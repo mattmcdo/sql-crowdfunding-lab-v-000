ddbbbbaca3ee6d1d02c271ed9ea3dfe311fc5fcd
Matthew McDonald <matthew.mcd1031@gmail.com>
2018-04-07 20:36:30 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -10,7 +10,7 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
 "SELECT projects.title, pledges.amount
 FROM projects
 LEFT JOIN pledges
-ON projects.title = pledges.project_id ORDER BY projects.title ASC"
+ON projects.title = pledges.amount ORDER BY projects.title ASC"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
