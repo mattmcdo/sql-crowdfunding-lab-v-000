e40f98fabe05586d9466d69b6dd2e8227f89359c
Matthew McDonald <matthew.mcd1031@gmail.com>
2018-04-07 20:54:25 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -10,12 +10,16 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
 "SELECT projects.title, pledges.amount
 FROM projects
 LEFT JOIN pledges
-ON projects.title = pledges.project_id SUM(pledge.amount)
+ON projects.title = pledges.project_id 
 ORDER BY projects.title"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"Write your SQL query Here"
+"SELECT users.name, users.age, pledges.amount
+FROM users
+LEFT JOIN pledges
+ON users.name = pledges.amount
+ORDER BY users.name"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
