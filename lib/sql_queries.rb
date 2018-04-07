37815ce69eb480d567959ba7757d6be7af5a8918
Matthew McDonald <matthew.mcd1031@gmail.com>
2018-04-07 21:02:56 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -18,7 +18,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 "SELECT users.name, users.age, pledges.amount
 FROM users
 LEFT JOIN pledges
-ON users.name = SUM(pledges.amount) FROM pledges
+ON users.name = pledges.user_id
 ORDER BY users.name"
 end
 
