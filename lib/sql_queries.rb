fa1611d1196e16f68611201c92e8d9ceb6ddc5de
Matthew McDonald <matthew.mcd1031@gmail.com>
2018-04-07 21:36:45 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,10 +23,10 @@ GROUP BY users.name"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT projects.title, SUM(pledges.amount)- projects.funding_goal)
+"SELECT projects.title, SUM(pledges.amount)- projects.funding_goal) AS amount_left
 FROM projects
 JOIN pledges
-ON projects.id = pledges.project_id HAVING pledges.amount >= 0"
+ON projects.id = pledges.project_id HAVING amount_left >= 0"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
